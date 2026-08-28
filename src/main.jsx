763d69d476a5f32a01bcb57ffdd06d5013ca9eb2
ronaldstech <liwewejacob265@gmail.com>
2026-08-28 17:41:54 +0200

    fixed dashboard css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider } from './context/AuthContext';
 import './styles/index.css';
+import './styles/auth.css';
+import './styles/dashboard.css';
+import './styles/profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
